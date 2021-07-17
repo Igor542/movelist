7e8cb2e790a86a4fb32eac2ee4c898f1c040f0a2
evatux <evatux@gmail.com>
2021-07-16 22:04:29 -0700

    fixup

--- movlist/__init__.py
+++ movlist/__init__.py
@@ -31,10 +31,8 @@ def create_app(test_config=None):
     movlist.db.init_app(app)
 
     # apply the blueprints to the app
-    from flaskr import auth, blog
-
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    app.register_blueprint(movlist.auth.bp)
+    app.register_blueprint(movlist.blog.bp)
 
     app.add_url_rule("/", endpoint="index")
 
